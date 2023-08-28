d5b9510105826c0d83e0217c64e6a397847f0c2a
chulhwan99 <chulhwankim99@gmail.com>
2023-08-28 17:45:30 +0900

    #2023-2-nhn-git/1 스켈레톤 작성.

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
-public class Main(){
+public class Main {
+
+    public double add(double x, double y){
+        return 1;
+    }
+    public double sub(double x, double y){
+        return 1;
+    }
+    public double multi(double x, double y){
+        return 1;
+    }
+    public double div(double x, double y){
+        return 1;
+    }
     public static void main(String[] args) {
-        
+        System.out.println("Hello world!");
     }
 }
