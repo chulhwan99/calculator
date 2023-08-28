2fd2d4d8e1fb79b53323fee9951f60bd49a706c0
chulhwan99 <chulhwankim99@gmail.com>
2023-08-28 18:07:48 +0900

    #2023-2-nhn-git 메인함수 입력값 받는 부분 작성

--- Main.java
+++ Main.java
@@ -1,18 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
 
-    public double add(double x, double y){
+    public static double add(double x, double y){
         return 1;
     }
-    public double sub(double x, double y){
+    public static double sub(double x, double y){
         return 1;
     }
-    public double multi(double x, double y){
+    public static double multi(double x, double y){
         return 1;
     }
-    public double div(double x, double y){
+    public static double div(double x, double y){
         return 1;
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("please enter a expression : ");
+
+        Scanner scan = new Scanner(System.in);
+        double num1,num2,ans=0;
+        char operator;
+        num1 = scan.nextDouble();
+        operator = scan.next().charAt(0);
+        num2 = scan.nextDouble();
+
+        
+        System.out.println("정답은 : " + ans);
     }
 }
